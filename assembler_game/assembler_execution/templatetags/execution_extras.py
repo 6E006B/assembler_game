20913c1c4f16b16271c3107cb478cfafc64dfaf3
n00k <christopher.schwardt@gmx.de>
2017-03-30 15:37:29 +0200

    add get_item and translate_eflags filters

---
+++ assembler_game/assembler_execution/templatetags/execution_extras.py
@@ -0,0 +1,40 @@
+from django import template
+
+register = template.Library()
+
+
+@register.filter
+def get_item(dictionary, key):
+    return dictionary.get(key)
+
+eflags_dict = {
+    0: 'CF',
+    2: 'PF',
+    4: 'AF',
+    6: 'ZF',
+    7: 'SF',
+    8: 'TF',
+    9: 'IF',
+    10: 'DF',
+    11: 'OF',
+    12: 'IOPL1',
+    13: 'IOPL2',
+    14: 'NT',
+    16: 'RF',
+    17: 'VM',
+    18: 'AC',
+    19: 'VIF',
+    20: 'VIP',
+    21: 'ID',
+}
+
+@register.filter
+def translate_eflags(eflags):
+    flags = []
+    counter = 0
+    for bit in bin(eflags)[:1:-1]:
+        if bit == '1':
+            if counter in eflags_dict:
+                flags.append(eflags_dict[counter])
+        counter += 1
+    return " ".join(flags)
